ca01b616b167ab0a744d7565522ea2ca3842a207
Ayush yadav <20ayush07yadav03@gmail.com>
2022-09-29 23:54:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <>
+      <Router>
+        <Navbar />
+        
+        <Routes>
+        <Route path='/' exact component={Home} />
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
