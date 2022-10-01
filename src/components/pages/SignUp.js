deb11deda248c3a60ebc1a7de32b99c73c29ba8e
Ayush yadav <20ayush07yadav03@gmail.com>
2022-10-01 23:01:52 +0530

    completed

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../../App.css'
+import Footer from '../Footer'
 
 function SignUp() {
   return (
     <>
         <h1 className='sign-up'>SIGN UP</h1>
+        <Footer />
     </>
   )
 }
